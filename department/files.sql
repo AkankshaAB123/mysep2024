a73d12f280bdde8bc4201586731748f2823bf7cd
AkankshaAB123 <akankshaashok2895@gmail.com>
2024-09-09 15:59:07 +0530

    Create files.sql

---
+++ department/files.sql
@@ -0,0 +1,11 @@
+-- departments (id,name)
+CREATE TABLE departments
+(   
+    id INT PRIMARY KEY auto_increment,
+    name VARCHAR(100) NOT NULL
+);
+DESC departments;
+SELECT * FROM departments;
+INSERT INTO departments(name)
+VALUES ('Analysis'),('Design'),('Development'),('Testing');
+    
