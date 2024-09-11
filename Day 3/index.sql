f392fda99da326c715b232987a911df68294ba6c
AkankshaAB123 <akankshaashok2895@gmail.com>
2024-09-11 22:53:50 +0530

    Create index.sql

---
+++ Day 3/index.sql
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    content="width=device-width,"
+    <title>My Manufacturing</title>
+    <LINK REL="stylesheet" HREF="styles.css" TYPE="text/css"/>
+</head>
+<body>
+    <h1>My Manufacturing</h1>
+    <div>
+        <a href="index.html">Home</a>
+        <a href="about.html">About Us</a>
+    </div>
+    <h3>Home Page</h3>
+    <p>This is going to be website for car manufacturing company.</p>
+    <h4>Our Models</h4>
+    <UL>
+        <LI>SUV</LI>
+        <LI>SEDAN</LI>
+        <LI>Electric Car</LI>
+    </UL>
+    <h5>Features</h5>
+    <OL>
+        <LI>Eco-Friendly Cars</LI>
+        <LI>High Performance Engines</LI>
+        <LI>Top-tier safety standard</LI>
+    </OL>
+    <h6>Our car</h6>
+    <img src="car.jpeg" alt="Our Car" width="200"/>
+
+    <h3>Our advertisement</h3>
+    <video controls>
+        <source src="sedan.mp4" type="video/mp4" width="200"/>
+        Our ad file cannot be run in your browser.
+    </video>
+</body>
+</html>
